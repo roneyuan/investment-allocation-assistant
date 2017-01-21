dbd56f201c404d3c7612252944490baf432f7f18
roneyuan <roneyuan_m03@hotmail.com>
2017-01-21 14:43:06 -0500

    Add result calculate function

--- app.js
+++ app.js
@@ -2,52 +2,52 @@ var question = [
 	{
 		id: 1,
 		question: '1. This is the first',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 2,
 		question: '2. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 3,
 		question: '3. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 4,
 		question: '4. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 5,
 		question: '5. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 6,
 		question: '6. This is the first',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 7,
 		question: '7. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 8,
 		question: '8. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 9,
 		question: '9. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	},
 	{
 		id: 10,
 		question: '10. ',
-		choice: [1,2,3,4]
+		choice: ["1. ","2. ","3. ","4. "]
 	}	
 ];
 
@@ -94,18 +94,11 @@ var answer = [
 	}	
 ];
 
-// Listner for the user's choice
-
-// DOM Manipulation
-// var template = "<div class='frame'> \
-					// <div class='question'>Ask</div> \
-					// <form class='choice'> \
-						// <button class='submit'>Submit</button> \
-					// </form> \
-				// </div>";
-
-				
-				
+var allocation = {
+	s: 50,
+	b: 50
+}
+		
 function start() {
 	$('.begin').on('click', function(event) {
 		event.preventDefault();
@@ -130,7 +123,7 @@ function startQuestion(number) {
 	
 	$(".submit").on('click', function(event) {
 		event.preventDefault();
-		getAnswer(number);
+		getInputAnswer(number);
 	});		
 }
 
@@ -140,7 +133,7 @@ function generateQuestion(number) {
 	
 	for (var n=0; n < 4; n++) {
 		choices += "<input type='radio' name='" + currentObj['id'] + 
-					"' value='" + currentObj['choice'][n] + "' /> " +
+					"' value='" + n + "' /> " +
 					currentObj['choice'][n] + " <br>";
 	}
 
@@ -158,10 +151,9 @@ function goToNextQuestion(number) {
 	} else {
 		startQuestion(number);	
 	}
-	
 }
 
-function getAnswer(number) {
+function getInputAnswer(number) {
 	// Get the user's choice
 	var inputName = "input[name=" + question[number]['id'] + "]:checked"
 	var thisAnswer = $(inputName, '.choice').val();
@@ -171,6 +163,7 @@ function getAnswer(number) {
 		return;
 	} else {
 		saveAnswer(number, thisAnswer);
+		changeChart(number, thisAnswer);
 		var next = number + 1;
 		goToNextQuestion(next);	
 	}
@@ -185,16 +178,255 @@ function saveAnswer(number, thisAnswer) {
 // Answer calculation
 function presentResult() {
 	var result = "";
+	
 	answer.forEach(function(element) {
 		result += "<div>"+element['id']+": "+element['answer']+"</div>";
 	});
 	$('.content').append(result);
 	
+	//var displayedNumber = calcResult();
+	//presentChart(displayedNumber);
+	presentChart();
+}
+/*
+function calcResult() {
+	var defaultSAllocation = 50;
+	
+	answer.forEach(function(element) {
+		switch (element['id']) {
+			case 1:
+				switch (element['answer']) {
+					case "0":
+						defaultSAllocation -= 10;		
+						break;
+					case "1":
+						defaultSAllocation -= 5;
+						break;
+					case "2":
+						defaultSAllocation += 5;
+						break;
+					case "3":
+						defaultSAllocation += 10;
+						break;
+					default:			
+						break;
+				}
+				break;
+			case 2:
+				break;
+			case 3:
+				switch (element['answer']) {
+					case "0":
+						defaultSAllocation -= 10;		
+						break;
+					case "1":
+						defaultSAllocation -= 5;
+						break;
+					case "2":
+						defaultSAllocation += 5;
+						break;
+					case "3":
+						defaultSAllocation += 10;
+						break;
+					default:			
+						break;
+				}
+				break;			
+			case 4:
+				break;
+			case 5:
+				switch (element['answer']) {
+					case "0":
+						defaultSAllocation -= 10;		
+						break;
+					case "1":
+						defaultSAllocation -= 5;
+						break;
+					case "2":
+						defaultSAllocation += 5;
+						break;
+					case "3":
+						defaultSAllocation += 10;
+						break;
+					default:			
+						break;
+				}
+				break;			
+			case 6:
+				break;
+			case 7:
+				switch (element['answer']) {
+					case "0":
+						defaultSAllocation -= 10;		
+						break;
+					case "1":
+						defaultSAllocation -= 5;
+						break;
+					case "2":
+						defaultSAllocation += 5;
+						break;
+					case "3":
+						defaultSAllocation += 10;
+						break;
+					default:			
+						break;
+				}
+				break;			
+			case 8:
+				break;
+			case 9:
+				switch (element['answer']) {
+					case "0":
+						defaultSAllocation -= 10;		
+						break;
+					case "1":
+						defaultSAllocation -= 5;
+						break;
+					case "2":
+						defaultSAllocation += 5;
+						break;
+					case "3":
+						defaultSAllocation += 10;
+						break;
+					default:			
+						break;
+				}
+				break;			
+			case 10:
+				break;
+			default:
+				break;
+		}
+	});
+	
+	return defaultSAllocation;
+}*/
+
+function presentChart() {
 	// D3.js here
+	// P here
+	
+	// But b first
+	console.log(allocation['s']);
+	console.log(allocation['b']);
+	
 }
 
-function changeChart() {
+function changeChart(thisQuestion, thisAnswer) {
 	// TODO
+	
+	var defaultSAllocation = allocation['s'];
+	switch (thisQuestion) {
+		case 1:
+			switch (thisAnswer) {
+				case "0":
+					defaultSAllocation -= 10;		
+					break;
+				case "1":
+					defaultSAllocation -= 5;
+					break;
+				case "2":
+					defaultSAllocation += 5;
+					break;
+				case "3":
+					defaultSAllocation += 10;
+					break;
+				default:			
+					break;
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			switch (thisAnswer) {
+				case "0":
+					defaultSAllocation -= 10;		
+					break;
+				case "1":
+					defaultSAllocation -= 5;
+					break;
+				case "2":
+					defaultSAllocation += 5;
+					break;
+				case "3":
+					defaultSAllocation += 10;
+					break;
+				default:			
+					break;
+			}
+			break;			
+		case 4:
+			break;
+		case 5:
+			switch (thisAnswer) {
+				case "0":
+					defaultSAllocation -= 10;		
+					break;
+				case "1":
+					defaultSAllocation -= 5;
+					break;
+				case "2":
+					defaultSAllocation += 5;
+					break;
+				case "3":
+					defaultSAllocation += 10;
+					break;
+				default:			
+					break;
+			}
+			break;			
+		case 6:
+			break;
+		case 7:
+			switch (thisAnswer) {
+				case "0":
+					defaultSAllocation -= 10;		
+					break;
+				case "1":
+					defaultSAllocation -= 5;
+					break;
+				case "2":
+					defaultSAllocation += 5;
+					break;
+				case "3":
+					defaultSAllocation += 10;
+					break;
+				default:			
+					break;
+			}
+			break;			
+		case 8:
+			break;
+		case 9:
+			switch (thisAnswer) {
+				case "0":
+					defaultSAllocation -= 10;		
+					break;
+				case "1":
+					defaultSAllocation -= 5;
+					break;
+				case "2":
+					defaultSAllocation += 5;
+					break;
+				case "3":
+					defaultSAllocation += 10;
+					break;
+				default:			
+					break;
+			}
+			break;			
+		case 10:
+			break;
+		default:
+			break;
+	}	
+	
+	
+	var b = 100 - defaultSAllocation;
+	
+	allocation['s'] = defaultSAllocation;
+	allocation['b'] = b;
+	// Update here...
 	
 	return;
 }
